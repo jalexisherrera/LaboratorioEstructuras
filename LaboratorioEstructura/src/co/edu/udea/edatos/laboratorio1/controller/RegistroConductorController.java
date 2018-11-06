274d6a801f0645f5c08292dd621cc0333e5f53ff
Alexis Herrera <alexizher@gmail.com>
2018-11-05 21:55:49 -0500

    Se agregan las clases Controller necesarias

---
+++ LaboratorioEstructura/src/co/edu/udea/edatos/laboratorio1/controller/RegistroConductorController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.udea.edatos.laboratorio1.controller;
+
+/**
+ *
+ * @author Alexis Herrera
+ */
+public class RegistroConductorController {
+/*   
+   Conductor conductor;
+   
+   ConductorDAO conductorDAO = new FileConductorDAO();
+   
+*/
+}
